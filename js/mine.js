fce23946d871fa5935394299ff389879df377f6b
Rostislav <Rostislav.086@mail.ru>
2020-07-09 22:51:35 +0500

    D_2

--- js/mine.js
+++ js/mine.js
@@ -1,5 +1,6 @@
 "use strict";
 // Получаем данные со страницы ================================================
+const db = JSON.parse(localStorage.getItem("Garage")) || [];
 
 const btnAdd = document.querySelector(".add__ad"),
   modalAdd = document.querySelector(".modal__add"),
@@ -7,44 +8,56 @@ const btnAdd = document.querySelector(".add__ad"),
   modalClose = document.querySelector(".modal__close"),
   modalBtnSubmit = document.querySelector(".modal__btn-submit"),
   modalSubmit = document.querySelector(".modal__submit"),
+  modalBtnWarning = document.querySelector(".modal__btn-warning"),
+  catalog = document.querySelector(".catalog"),
   card = document.querySelector(".card");
 
+const elementsModalSubmit = [...modalSubmit.elements].filter(
+  elem => elem.tagName !== "BUTTON"
+);
+
 // Объявление функций =======================================================
+
+const saveDb = () => localStorage.setItem("Garage", JSON.stringify(db));
+console.log(db);
 
 const openModalAdd = () => {
   if (modalAdd.classList.contains("hide")) {
     modalAdd.classList.remove("hide");
     modalBtnSubmit.disabled = true;
+    document.addEventListener("keydown", closeModal);
   }
 };
 
-const openModalItem = () => {
-  modalItem.classList.remove("hide");
-};
-
-const closeModalItem = event => {
+const openModalItem = event => {
   const target = event.target;
 
-  if (
-    target.classList.contains("modal__close") ||
-    target === modalItem ||
-    event.keyCode === 27
-  ) {
-    modalItem.classList.add("hide");
+  if (target.closest(".card")) {
+    modalItem.classList.remove("hide");
+    document.addEventListener("keydown", closeModal);
   }
 };
 
-const closeModalAdd = event => {
+const closeModal = event => {
   const target = event.target;
 
   if (
-    target.classList.contains("modal__close") ||
-    target === modalAdd ||
-    event.keyCode === 27
+    target.closest(".modal__close") ||
+    target.classList.contains("modal") ||
+    event.code === "Escape"
   ) {
     modalAdd.classList.add("hide");
+    modalItem.classList.add("hide");
     modalSubmit.reset();
+    document.removeEventListener("keydown", closeModal);
+    checkForm();
   }
+};
+
+const checkForm = () => {
+  const validForm = elementsModalSubmit.every(elem => elem.value);
+  modalBtnSubmit.disabled = !validForm;
+  modalBtnWarning.style.display = validForm ? "none" : "";
 };
 
 // Вызов функций ==============================================================
@@ -52,12 +65,18 @@ const closeModalAdd = event => {
 // Обработчик событий =========================================================
 
 btnAdd.addEventListener("click", openModalAdd);
-modalAdd.addEventListener("click", closeModalAdd);
-card.addEventListener("click", openModalItem);
-modalItem.addEventListener("click", closeModalItem);
-document.addEventListener("keydown", closeModalAdd);
-document.addEventListener("keydown", closeModalItem);
+catalog.addEventListener("click", openModalItem);
+modalAdd.addEventListener("click", closeModal);
+modalItem.addEventListener("click", closeModal);
+modalSubmit.addEventListener("input", checkForm);
 
-document.addEventListener("click", event => {
-  console.log(event.target);
+modalSubmit.addEventListener("submit", event => {
+  event.preventDefault();
+  const itemObject = {};
+  for (const elem of elementsModalSubmit) {
+    itemObject[elem.name] = elem.value;
+  }
+  db.push(itemObject);
+  closeModal({ target: modalAdd });
+  saveDb(itemObject);
 });
