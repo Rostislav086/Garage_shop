b552295926686ab31b9bbad8733c44bb4193a4fb
Rostislav086 <Rostislav.086@mail.ru>
2020-07-10 08:35:05 +0500

    win

--- js/mine.js
+++ js/mine.js
@@ -1,17 +1,29 @@
 "use strict";
 
+const itemObject = {};
+
 const modalAdd = document.querySelector(".modal__add"),
   modalItem = document.querySelector(".modal__item"),
   modalClose = document.querySelector(".modal__close"),
   btnAdd = document.querySelector(".add__ad"),
   card = document.querySelector(".card"),
   modalSubmit = document.querySelector(".modal__submit"),
+  modalBtnSubmit = document.querySelector(".modal__btn-submit"),
   catalog = document.querySelector(".catalog");
+
+const elementSubmitForm = [...modalSubmit.elements].filter(
+  (elem) => elem.tagName !== "BUTTON"
+);
+
+console.log(elementSubmitForm);
 
 // Объявляем функции ==========================================================
 
 const openModalAdd = () => {
   modalAdd.classList.remove("hide");
+
+  modalBtnSubmit.disabled = true;
+
   document.addEventListener("keydown", closeModal);
 };
 
@@ -26,7 +38,6 @@ const openModalItem = (event) => {
 
 const closeModal = function (event) {
   const target = event.target;
-  console.log(event);
 
   if (
     target.classList.contains("modal__close") ||
@@ -43,9 +54,13 @@ const closeModal = function (event) {
   }
 };
 
+const validForm = () => {
+  const validForm = elementSubmitForm.every((elem) => console.log(elem.value));
+};
 // обработчики событий  =======================================================
 
 btnAdd.addEventListener("click", openModalAdd);
 catalog.addEventListener("click", openModalItem);
 modalItem.addEventListener("click", closeModal);
 modalAdd.addEventListener("click", closeModal);
+modalSubmit.addEventListener("input", validForm);
